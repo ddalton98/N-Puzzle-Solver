96ac0dbe97ef25762fd76ab29be286c089443029
Daniel Dalton <36508791+ddalton98@users.noreply.github.com>
2019-03-08 13:30:08 +0000

    Added extra validation
    Validation for checking individual digits

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.*;
 
 /**
 *	CS4006 A* Project
@@ -14,12 +15,28 @@ public class Main {
     }
 
     private static String inputWindow(){
+		boolean correct = true;
         String msg = "Please input a 9 digits(0-9) with a space separating each";
         String pattern1 = "^(?!.*(.).*\\1)*\\d{10}$";
-        String pattern2 = "^([0-9]{1}\\s{1}[0-9]{1}\\s{1}[0-9]{1}\\s{1}[0-9]{1}\\s{1}[0-9]{1}\\s{1}[0-9]{1}\\s{1}[0-9]{1}\\s{1}[0-9]{1}\\s{1}[0-9]{1}\\s{1}[0-9]{1})\\s*";
+        String pattern2 = "^([0-9]{1}\s+){10}\s*";
         String rawInput = JOptionPane.showInputDialog(null, msg, "8 Puzzle", JOptionPane.QUESTION_MESSAGE);
         String checkStr = rawInput.replaceAll("\\s", "");
         if(checkStr.matches(pattern1) && rawInput.matches(pattern2)){
+			String[] strInput = rawInput.split(" ");
+			int inputArr[] = new int[strInput.length];
+			
+			for(int i = 0; i < strInput.length; i++){
+				inputArr[i] = Integer.parseInt(strInput[i]);
+			}
+			int tempArr[] = inputArr;
+			Arrays.sort(tempArr);
+			
+			for(int i = 0; i < inputArr.length; i++){
+				if(tempArr[i] != i){
+					JOptionPane.showMessageDialog(null, "Error: Entry format incorrect", "Error", JOptionPane.ERROR_MESSAGE);
+					return inputWindow();
+				}
+			}
             return rawInput;
         }else {
             JOptionPane.showMessageDialog(null, "Error: Entry format incorrect", "Error", JOptionPane.ERROR_MESSAGE);
