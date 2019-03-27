a9e7d484d14a9a5fa8f0761ed018b5e652adb645
Nytrode <45094007+Nytrode@users.noreply.github.com>
2019-03-27 09:45:55 +0000

    Fixed getH & added check for finish

--- Board.java
+++ Board.java
@@ -1,46 +1,40 @@
 public class Board{
-  private static int[][] goal;
   private int g;
   private int value;
   private int [][] arr;
+  private int [][] goal;
 
 
-  public Board(int arr[][], int g){
+  public Board(int arr[][], int goal[][], int g){
     this.g = g;
     this.arr = arr;
-    this.value = g + getH(this.arr);
+	this.goal = goal;
+    this.value = g + getH();
   }
 
-  private int getH(int[][] arr){
-    int count;
-
-    for(int x = 0; x < arr.length; x++){
-      if(arr[x] != goal[x]){
-        count++;
-      }
-    }
+  private int getH(){
+    int count = 0;
+	
+	for(int x = 0; x < goal.length; x++){
+		for(int y = 0; y < goal[x].length; y++){
+			if(arr[x][y] != goal[x][y]) count++;
+		}
+	}
 
     return count;
   }
 
-  public boolean equals(int[][] check){
-    int count;
-
-    for(int x = 0; x < check.length; x++){
-      if(arr[x] != check[x]){
-        count++;
-      }
-    }
-
-    if(count == 0){
-      return true;
-    } else {
-      return false;
-    }
+  public boolean finish(){
+	  boolean ret = false;
+	  
+	  if(this.getH() == 0) ret = true;
+	  
+	  return ret;
+	
   }
 
-  public int[][] getBoard(){
-    reutrn arr;
+  public int[][] getTable(){
+    return arr;
   }
 
   public int getValue(){
@@ -49,5 +43,9 @@ public class Board{
 
   public int getG(){
     return g;
+  }
+  
+  public void setG(int val){
+	  g = val;
   }
 }
