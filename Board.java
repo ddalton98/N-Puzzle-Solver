4914adfa4c4615f7e5d02c5e0f3b21827edd4775
Nytrode <45094007+Nytrode@users.noreply.github.com>
2019-03-31 00:41:14 +0000

    added isEqual() method + printBoard()

--- Board.java
+++ Board.java
@@ -4,33 +4,46 @@ public class Board{
   private int [][] arr;
   private int [][] goal;
 
-
   public Board(int arr[][], int goal[][], int g){
     this.g = g;
     this.arr = arr;
-	this.goal = goal;
-    this.value = g + getH();
+	  this.goal = goal;
+    this.value = g + this.getH();
   }
 
   private int getH(){
     int count = 0;
-	
+
 	for(int x = 0; x < goal.length; x++){
-		for(int y = 0; y < goal[x].length; y++){
+		for(int y = 0; y < goal.length; y++){
 			if(arr[x][y] != goal[x][y]) count++;
 		}
-	}
-
+	  }
     return count;
   }
 
   public boolean finish(){
 	  boolean ret = false;
-	  
+
 	  if(this.getH() == 0) ret = true;
-	  
+
 	  return ret;
-	
+
+  }
+  
+  public boolean isEqual(Board a){
+	  int[][] temp = a.getTable();
+	  int count = 0;
+	  
+	  for(int x = 0; x < arr.length; x++){
+		for(int y = 0; y < arr.length; y++){
+			if(arr[x][y] == temp[x][y]) count++;
+		}
+	  }
+	  
+	  if(count == arr.length*arr.length){
+		  return true;
+	  } else return false;
   }
 
   public int[][] getTable(){
@@ -44,8 +57,18 @@ public class Board{
   public int getG(){
     return g;
   }
-  
+
   public void setG(int val){
 	  g = val;
+  }
+
+  public void printBoard(){
+	  for(int i = 0; i < arr.length; i++){
+		  for(int j = 0; j < arr.length; j++){
+			  System.out.print(arr[i][j] + " ");
+		  }
+		  System.out.println("");
+	  }
+	  System.out.println("");
   }
 }
